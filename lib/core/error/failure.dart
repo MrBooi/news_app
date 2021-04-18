12864349a7175e3bcbd8e063eb5b60151e472c22
Ayabonga <ayabongabooi2@gmail.com>
2021-04-18 19:51:25 +0200

    added core folder

---
+++ lib/core/error/failure.dart
@@ -0,0 +1,10 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'failure.freezed.dart';
+
+@freezed
+abstract class Failure with _$Failure {
+  const factory Failure.serverFailure() = ServerFailure;
+  const factory Failure.cacheFailure() = CacheFailure;
+  const factory Failure.socketFailure() = SocketFailure;
+}
